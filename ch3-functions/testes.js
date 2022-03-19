a23012b499cd3010f487740504688b6a8809f9e6
paulo-sk <paulobsb1337@gmail.com>
2022-03-19 16:43:44 -0300

    formula phi

--- ch3-functions/testes.js
+++ ch3-functions/testes.js
@@ -10,11 +10,24 @@ const power = function (base, exponent) {
 console.log(power(2, 3));
 
 //o ovo ou a galinha
-function chicken() {
+/* function chicken() {
     return egg();
 }
 function egg() {
     return chicken();
 }
-console.log(chicken() + " came first.");
+console.log(chicken() + " came first."); */
 
+
+//recursion (stackoverflow)
+function primeiraVaga(){
+    console.log('para trabalhar vc precisa de xp');
+    return experiencia()
+}
+
+function experiencia(){
+    console.log('pra ter xp vc precisa trabalhar');
+    return primeiraVaga()
+}
+
+console.log(primeiraVaga())
--- ch4-objects-and-arrays/squirrel/squirrel.js
+++ ch4-objects-and-arrays/squirrel/squirrel.js
@@ -2,17 +2,29 @@
 let journal = []
 
 function addEntry(events, squirrel){
+    //esses parametros sao os nomes das propriedades do objeto, os argumentos passados sao os valores
     journal.push({events, squirrel})
+
 }
 
 addEntry(["work","touched tree","pizza","running","television"], false)
 addEntry(["work", "ice cream", "cauliflower", "lasagna","touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts","beer"], true);
 
-console.table(journal);
+console.log(journal);
 
 /*o valor da formula, que calcula a correlacao de cada variavel de evento com squirrel
 é sempre entre -1 e 1, onde -1 é uma correlacao negativa e 1 é uma correlacao positiva 
 0 é quando nao tem correleçao*/
+table = [76,9,4,1] //00, 01, 10, 11
 
+function phi(table) {
+    return (table[3]*table[0] - table[2]*table[1]) /
+    Math.sqrt((table[2] + table[3])*
+    (table[0] + table[1])*
+    (table[1] + table[3])*
+    (table[0] + table[2]))
+    
+}
 
+console.log(phi(table))
